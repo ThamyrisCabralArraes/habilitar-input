471232023cef82280b6f4236414da132d7c8c383
Thamyris <thamyriscabral@hotmail.com>
2022-11-05 15:04:06 -0300

    final commit

--- js/newsletter.js
+++ js/newsletter.js
@@ -7,5 +7,14 @@ function aoSair() {
   txtEmail.disabled = true
 }
 
-
-
+const contra = document.querySelector('#contra')
+const botao = document.querySelector('#botao')
+function contratoo() {
+  contra.checked = true
+  botao.disabled = false
+}
+function sair() {
+  contra.checked = false
+  botao.disabled = true
+}
+console.log(contratoo)
