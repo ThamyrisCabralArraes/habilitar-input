ca794458ff1e50fa7b96a35054dde14c2b2d4519
Thamyris <thamyriscabral@hotmail.com>
2022-11-22 10:54:53 -0300

    Add botao ligth e dark

--- js/newsletter.js
+++ js/newsletter.js
@@ -1,20 +1,39 @@
-const txtEmail = document.getElementById("txtEmail")
+const txtEmail = document.getElementById('txtEmail');
 function aoClicar() {
-  txtEmail.disabled = false
-  txtEmail.focus()
+  txtEmail.disabled = false;
+  txtEmail.focus();
 }
 function aoSair() {
-  txtEmail.disabled = true
+  txtEmail.disabled = true;
 }
-// DESAFIO 
-const contra = document.querySelector('#contra')
-const botao = document.querySelector('#botao')
-botao.disabled = true
+// DESAFIO
+const contra = document.querySelector('#contra');
+const botao = document.querySelector('#botao');
+botao.disabled = true;
 
 function contratoo() {
-  botao.disabled = !contra.checked
+  botao.disabled = !contra.checked;
 }
+
 // function sair() {
 //   contra.checked = false
 //   botao.disabled = true
 // }
+
+let tela = document.querySelector('main');
+
+let btdark = document.querySelector('#btdark');
+let btligth = document.querySelector('#btligth');
+
+btdark.addEventListener('click', modoDark);
+btligth.addEventListener('click', modoLigth);
+
+function modoDark() {
+  tela.classList.add('btdark');
+  tela.classList.remove('btligth');
+}
+
+function modoLigth() {
+  tela.classList.add('btligth');
+  tela.classList.remove('btdark');
+}
